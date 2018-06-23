a46e8c706094236252745f86f85e2fcd5c88d82f
Balou9 <emmanuel.owosekun@gmail.com>
2018-06-24 00:03:07 +0200

    'morem'

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
-function igrp (arr, pattern, value) {
+//  control
+
+function igraber (arr, pattern, value) {
   var result = []
     for (var i = 0; i < arr.length; i++) {
       if (pattern === arr[i] && value === true) {
@@ -12,5 +14,9 @@ function igrp (arr, pattern, value) {
   return result
 }
 
+function igrp (arr, pattern, value, callback) {
+  if (!arr) return callback(err)
+  callback(null, igraber(arr, pattern, value))
+}
 
 module.exports = igrp
--- usage.js
+++ usage.js
@@ -1,14 +1,7 @@
 var igrp = require('./index.js')
 var names = ['Mikey', 'Allday', 'Mikey','Everyday', '419', 'Allday', '419']
 
-function testIgrp (arr) {
-  return {
-    'defaultIgrp1': igrp(arr, 'Everyday'),
-    'defaultIgrp2': igrp(arr, '419'),
-    'valueIgrp1': igrp(arr, 'Everyday', true),
-    'valueIgrp2': igrp(arr, '419', true)
-  }
-}
 
-console.log(testIgrp(names))
-// console.log(igrp(names, 'Everyday'))
+igrp(names, 'Everyday', true, function(err, data) {
+  console.log(data)
+})
